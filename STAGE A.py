75d7ec17820659b55c8e78489a0a3532c838b922
Samantha-June <117636278+Samantha-June@users.noreply.github.com>
2022-11-22 15:37:12 +0200

    Add files via upload

---
+++ STAGE A.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import pandas as pd 
+
+
+# In[8]:
+
+
+df = pd.read_csv("Desktop\FoodBalanceSheets_E_Africa_NOFLAG.csv", encoding = "latin-1")
+
+
+# In[9]:
+
+
+correlations = Element Code.corr()
+print(correlations)
+
+
+# In[10]:
+
+
+Element Code.corr()
+
+
+# In[13]:
+
+
+print(correlations["Years"])
+
+
+# In[14]:
+
+
+corr_matrix = df.corr()
+corr_matrix
+
+
+# In[21]:
+
+
+pd.Dataframe(1st,columns = col,index = [1,2,3])
+
+
+# In[23]:
+
+
+df.info()
+
+
+# In[25]:
+
+
+df_missing = df.isna()
+df_missing.sum()
+
+
+# In[26]:
+
+
+my_tuppy = (1,2,5,8)
+
+my_tuppy[2] = 6
+
+
+# In[27]:
+
+
+array  = ([[94, 89, 63],
+
+             [93, 92, 48],
+
+             [92, 94, 56]])
+
+
+# In[28]:
+
+
+S = [['him', 'sell'], [90, 28, 43]]
+
+S[0][1][1]
+
+
+# In[36]:
+
+
+df.describe()
+
+
+# In[37]:
+
+
+df.describe()
+
+
+# In[39]:
+
+
+df.sum(years)
+
+
+# In[ ]:
+
+
+
+
